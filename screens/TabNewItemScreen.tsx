007e37b43fc8b22dc8f67d239986f696b11aa899
Jose Luis Gordillo Relaño <igorej@hotmail.com>
2023-01-29 17:49:42 +0100

    Update on Add new item

--- screens/TabNewItemScreen.tsx
+++ screens/TabNewItemScreen.tsx
@@ -1,26 +1,38 @@
 import { FontAwesome } from "@expo/vector-icons";
-import React from "react";
-import { Pressable, StyleSheet, TextInput, View } from "react-native";
+import React, { useEffect } from "react";
+import { Pressable, StyleSheet, TextInput, View, Keyboard, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { Button, Icon } from "react-native-elements";
 import { Text, StyledView } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { getStoredData, INITIAL_CONFIG_KEY } from "../utils/utils";
 
 export default function TabNewItemScreen({ navigation }: RootTabScreenProps<"TabAddNewItem">) {
   const [quantity, setQuantity] = React.useState<string>("");
+  const [code, setCode] = React.useState<string>("USD");
+  const [categoryType, setCategoryType] = React.useState<string>('expenses');
+
+  useEffect(() => {
+    fetchConfig();
+  }, []);
+
 
   const handleRemove = () => {
 
   }
   const formatNumber = (number: string) => {
     // function that receives an string and format to allow only numbers and one dot
+    var re = new RegExp(code, "g");
+    let nValue = number.replace(re, "").trim();
     const regex = /^(\d+)?(\.\d{0,2})?$/;
-    if (regex.test(number)) {
-      setQuantity(number);
+    if (regex.test(nValue)) {
+      setQuantity(nValue);
     }
-
-
   }
-
-
+  const fetchConfig = async () => {
+    //await initialiceCategories();
+    const config = await getStoredData(INITIAL_CONFIG_KEY)
+    setCode(config.currency.currency.code)
+  }
   const getTodayDate = () => {
     // return string with name of the day (Monday, tuesday...), the day number, and month of today
     const today = new Date();
@@ -32,6 +44,9 @@ export default function TabNewItemScreen({ navigation }: RootTabScreenProps<"Tab
 
 
     return `${dayNames[day]}, ${dayNumber} ${monthNames[month]}`;
+  }
+  const saveItem = () => {
+    // save the new item
   }
   navigation.setOptions({
     headerLeft: () => (
@@ -65,39 +80,84 @@ export default function TabNewItemScreen({ navigation }: RootTabScreenProps<"Tab
   });
   return (
     <StyledView style={styles.container}>
-      <Text style={styles.title}></Text>
-      <TextInput
-        placeholder={`Quantity`}
-        value={quantity}
-        keyboardType='numeric'
-        style={[styles.input, { borderWidth: 1, borderColor: '#fff', borderRadius: 5, backgroundColor: '#000' }]}
-        onChangeText={formatNumber}
-      />
-      <StyledView style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={{ flex: 1, }}>
+          <TextInput
+            placeholder={`Quantity`}
+            value={code + " " + quantity}
+            keyboardType='numeric'
+            style={[styles.input, { borderWidth: 1, borderColor: '#fff', borderRadius: 5, backgroundColor: '#000', width: "100%", color: '#fff' }]}
+            onChangeText={formatNumber}
+          />
+          <View style={{ padding: 5 }}>
+            <Text style={styles.title}>Category</Text>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                categoryType === 'expenses' && styles.selectedButton
+              ]}
+              onPress={() => setCategoryType('expenses')}
+            >
+              <View style={{ display: "flex", justifyContent: "space-between", alignItems: "center", flexDirection: "row" }}>
+                <Text>Expenses</Text>
+                <Text>{categoryType === "expenses" && <Icon type="entypo" name="check" color="#f9a825" />}</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                categoryType === 'incomes' && styles.selectedButton
+              ]}
+              onPress={() => setCategoryType('incomes')}
+            >
+              <View style={{ display: "flex", justifyContent: "space-between", alignItems: "center", flexDirection: "row" }}>
+                <Text>Incomes</Text>
+                <Text>{categoryType === "incomes" && <Icon type="entypo" name="check" color="#f9a825" />}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <StyledView style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+          {/* Button with 'Save' */}
+          <Button title="Create" onPress={saveItem} style={{ width: '100%', alignSelf: "flex-end", padding: 10 }} />
+        </View>
+
+      </TouchableWithoutFeedback>
+
+
     </StyledView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
+    width: "100%",
+    height: "100%",
   },
   input: {
     borderWidth: 1,
     fontSize: 20,
     lineHeight: 0,
     padding: 20,
-    width: "100%",
-    color: '#fff'
+    color: '#fff',
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    paddingBottom: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
+  }, button: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: 'gray',
+  },
+  selectedButton: {
+    backgroundColor: '#0C8266',
   },
 });
